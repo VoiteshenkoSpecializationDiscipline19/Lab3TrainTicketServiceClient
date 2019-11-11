aed70856be626d86c136e2c1256479ea54eaa591
Mikita Ishchanka <nikitai100300100300@gmail.com>
2019-11-12 00:23:30 +0300

    first version

---
+++ Tickets/Tickets/Tickets/Data/TicketsContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Tickets.Models
+{
+    public class TicketsContext : DbContext
+    {
+        public TicketsContext (DbContextOptions<TicketsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Tickets.Models.Ticket> Ticket { get; set; }
+    }
+}
---
+++ Tickets/Tickets/Tickets/Models/Ticket.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace Tickets.Models
+{
+    public class Ticket
+    {
+        public int Id { get; set; }
+        public string From { get; set; }
+        public string To { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime When { get; set; }
+        
+        public decimal Price { get; set; }
+    }
+}
